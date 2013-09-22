c54334617de35e33e4dbb9bc956eff497d260692
Luke Metz <luke.metz@students.olin.edu>
2013-09-22 17:54:01 -0400

    removed unneed headers

--- c/servo.c
+++ c/servo.c
@@ -1,21 +1,18 @@
 #include <p24FJ128GB206.h>
 #include "config.h"
 #include "common.h"
-#include "ui.h"
 #include "timer.h"
 #include "pin.h"
-#include "uart.h"
 #include "oc.h"
 #include "usb.h"
 #include <stdio.h>
 
-uint8_t string[40];
 #define SET_VALS 0
-uint16_t pan;
-uint16_t tilt;
 
 void VendorRequests(void) {
     WORD temp;
+    uint16_t pan = 0;
+    uint16_t tilt = 0;
 
     switch (USB_setup.bRequest) {
         case SET_VALS:
@@ -47,14 +44,10 @@ void VendorRequestsOut(void) {
 
 int16_t main(void) {
     init_clock();
-    init_uart();
-    init_ui();
     init_timer();
     init_pin();
     init_oc();
 
-    pan = 0;
-    tilt = 0;
     oc_servo(&oc1, &D[2], &timer1, 20e-3, 7e-4, 2.3e-3, (uint16_t)(0.9*65536));
     oc_servo(&oc2, &D[3], &timer2, 20e-3, 7e-4, 2.3e-3, (uint16_t)(0.9*65536)); //try using the same timer
 
