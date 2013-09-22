8589973b7021fc6d735f76ced5974d3f2d6a67df
Luke Metz <luke.metz@students.olin.edu>
2013-09-22 17:39:31 -0400

    removed trash

--- servo.c
+++ servo.c
@@ -11,30 +11,19 @@
 
 uint8_t string[40];
 #define SET_VALS 0
-#define GET_VALS 1
-uint16_t val1;
-uint16_t val2;
+uint16_t pan;
+uint16_t tilt;
 
 void VendorRequests(void) {
     WORD temp;
 
     switch (USB_setup.bRequest) {
         case SET_VALS:
-            val1 = USB_setup.wValue.w;
-            val2 = USB_setup.wIndex.w;
-            pin_write(&D[2], val1);
-            pin_write(&D[3], val2);
+            pan = USB_setup.wValue.w;
+            tilt = USB_setup.wIndex.w;
+            pin_write(&D[2], pan);
+            pin_write(&D[3], tilt);
             BD[EP0IN].bytecount = 0;    // set EP0 IN byte count to 0
-            BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
-            break;
-        case GET_VALS:
-            temp.w = val1;
-            BD[EP0IN].address[0] = temp.b[0];
-            BD[EP0IN].address[1] = temp.b[1];
-            temp.w = val2;
-            BD[EP0IN].address[2] = temp.b[0];
-            BD[EP0IN].address[3] = temp.b[1];
-            BD[EP0IN].bytecount = 4;    // set EP0 IN byte count to 4
             BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
             break;
         default:
@@ -64,8 +53,8 @@ int16_t main(void) {
     init_pin();
     init_oc();
 
-    val1 = 0;
-    val2 = 0;
+    pan = 0;
+    tilt = 0;
     oc_servo(&oc1, &D[2], &timer1, 20e-3, 7e-4, 2.3e-3, (uint16_t)(0.9*65536));
     oc_servo(&oc2, &D[3], &timer2, 20e-3, 7e-4, 2.3e-3, (uint16_t)(0.9*65536)); //try using the same timer
 
